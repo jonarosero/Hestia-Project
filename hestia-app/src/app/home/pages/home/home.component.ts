d183dbbb53eb0e8e78c7f4d3b999e2e8100cc223
Bruno Esparza <bruno.be81@gmail.com>
2020-06-22 22:03:13 -0500

    feat(home): add landing style

--- hestia-app/src/app/home/pages/home/home.component.ts
+++ hestia-app/src/app/home/pages/home/home.component.ts
@@ -1,18 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'hestia-home',
-  template: `
-    <p>
-      home works!
-    </p>
-  `,
-  styles: [
-  ]
+  templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(public readonly auth: AngularFireAuth) { }
 
   ngOnInit(): void {
   }

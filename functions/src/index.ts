7cbcf570104aae1dc87e620233f7df3d6e507f02
Bruno Esparza <bruno.be81@gmail.com>
2020-06-30 14:49:15 -0500

    feat(create solo): add funciton and callable to create a solo game

---
+++ functions/src/index.ts
@@ -0,0 +1,28 @@
+import { initializeApp } from 'firebase-admin';
+import * as functions from 'firebase-functions';
+
+const admin = initializeApp();
+const firestore = admin.firestore();
+const database = admin.database();
+
+export const createSoloGame = functions.https.onCall(async (data, context) => {
+    console.log(data);
+    const questionsRef = '/knowledge-areas/ingenieria-web/questions-blocks/MtsLtUc7Ecdr9IRZRyNp/questions';
+
+    const { docs, empty } = await firestore.collection(questionsRef).get();
+
+    if (empty) {
+        return null;
+    }
+
+    const questions = docs.map(d => {
+        const { answers, defaultWrongAnswerMessage, difficulty, id, image, questionText, time } = d.data();
+
+        return { answers, defaultWrongAnswerMessage, difficulty, id, image, questionText, time };
+    });
+    const user = context.auth?.uid;
+
+    const ref = await database.ref('single-player').push({ user, questions, status: 'STATING', created: Date.now() });
+
+    return ref.key;
+});
--- hestia-app/src/app/single-player/single-player-routing.module.ts
+++ hestia-app/src/app/single-player/single-player-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
+import { RouterModule, Routes } from '@angular/router';
 import { SinglePlayerComponent } from './single-player.component';
 
-const routes: Routes = [{ path: '', component: SinglePlayerComponent }];
+
+const routes: Routes = [
+  { path: ':id', component: SinglePlayerComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],

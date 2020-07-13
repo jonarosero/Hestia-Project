70888f71b47ba51e3abb0a42dc85253cba2cb9a6
Ascendere_Lab <ascendere_lab@iMac-de-Charlie.local>
2020-07-12 23:26:55 -0500

    feat(leader-board): design html

--- hestia-app/src/app/home/pages/leader-board/leader-board.component.ts
+++ hestia-app/src/app/home/pages/leader-board/leader-board.component.ts
@@ -1,18 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'hestia-leader-board',
-  template: `
-    <p>
-      leader-board works!
-    </p>
-  `,
+  templateUrl: './leader-board.component.html',
   styles: [
   ]
 })
 export class LeaderBoardComponent implements OnInit {
 
-  constructor() { }
+  constructor(public readonly auth: AngularFireAuth) { }
 
   ngOnInit(): void {
   }

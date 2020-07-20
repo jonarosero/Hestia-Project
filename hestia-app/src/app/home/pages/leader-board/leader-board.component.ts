bd93cd339d9accd25694977a6af6e5be4bb89fe3
Bruno Esparza <bruno.be81@gmail.com>
2020-07-20 11:23:11 -0500

    feat(leader board): add firestore an ui

--- hestia-app/src/app/home/pages/leader-board/leader-board.component.ts
+++ hestia-app/src/app/home/pages/leader-board/leader-board.component.ts
@@ -1,5 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { firestore } from 'firebase';
+import { Observable } from 'rxjs';
+
+export interface User {
+  disabled: boolean;
+  displayName: string;
+  email: string
+  joinDate: firestore.Timestamp;
+  photoURL: string;
+  playedGames: number;
+  points: number;
+  uid: string;
+}
 
 @Component({
   selector: 'hestia-leader-board',
@@ -9,7 +23,12 @@ import { AngularFireAuth } from '@angular/fire/auth';
 })
 export class LeaderBoardComponent implements OnInit {
 
-  constructor(public readonly auth: AngularFireAuth) { }
+  constructor(
+    public readonly auth: AngularFireAuth,
+    private readonly firestore: AngularFirestore,
+  ) { }
+
+  public readonly leaderBoard: Observable<Array<User>> = this.firestore.collection<User>('users', q => q.orderBy('playedGames', 'desc')).valueChanges();
 
   ngOnInit(): void {
   }
